6e38116b6d0c41c6c1a41ddc1d7283dc73bae5fe
Marcus Whitmore <marcus.whitmore@gmail.com>
2019-04-06 23:18:19 -0500

    homework

---
+++ trivia.js
@@ -0,0 +1,40 @@
+var buttonAlert;
+
+$("button").on("click", function (){
+
+
+});
+
+setTimeout( () => {
+  document.getElementById("choices").innerHTML = "New text";
+  document.write // will maybe work?
+  console.log('Hello')
+},
+  4 * 1000
+);
+
+
+var audio = new Audio("final.mp3");
+
+setTimeout(fiveSeconds, 5000);
+
+
+function fiveSeconds() {
+  $("#time-left").append("<h2>About 10 Seconds Left!</h2>");
+  console.log("10 seconds left");
+  setTimeout(tenSeconds, 5000);
+}
+
+function tenSeconds() {
+  $("#time-left").append("<h2>About 5 Seconds Left!</h2>");
+  console.log("5 seconds left");
+  setTimeout(timeUp, 5000);
+}
+
+function timeUp() {
+  console.log("done");
+  $("#time-left").append("<h2>Time's Up!</h2>");
+  console.log("time is up");
+  //  The following line will play the audio file we linked to above:
+  audio.play();
+}
